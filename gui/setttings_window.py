76d09262d5d275119429b4681becd9a2975aa3b5
Anthony21Avila <styler.espectral21@gmail.com>
2025-08-02 12:37:43 -0400

    Combobox y Slider

--- gui/setttings_window.py
+++ gui/setttings_window.py
@@ -1,5 +1,6 @@
-from tkinter import *
+from tkinter import Label, Tk, Button, DoubleVar, Scale, ttk
 
+#Variables de Inicio
 raiz = Tk()
 
 raiz.title("Traductor de Texto en Ventana")
@@ -9,6 +10,10 @@ raiz.geometry("800x600")
 
 raiz.config(background="gray")
 
+#transparent_color = "black"
+#raiz.wm_attributes('-transparentcolor', transparent_color)
+
+#Funcion de Inicio
 def Star_End():
     texto = btn_star["text"]
     if texto == "Star Translation":
@@ -19,10 +24,31 @@ def Star_End():
         btn_star.config(text="Star Translation")
         
 
-btn_inicio = Label(raiz, text="Screen Translator")
-btn_inicio.pack()
+#Label de Titulo
+titule = Label(raiz, text="Screen Translator")
+titule.pack()
 
+#Combobox para idiomas
+options = ["Inglish","Spanish", "French", "Chinese", "Japanese", "Korean", "German", "Russian"]
+labelIn = Label(raiz, text="Select Original Lenguaje", bg="gray")
+labelIn.pack(pady=(30, 0))
+lengaujeIn = ttk.Combobox(raiz, values=options)
+lengaujeIn.pack()
+
+labelOut = Label(raiz, text="Select Destiny Lenguaje", bg="gray")
+labelOut.pack(pady=(30, 0))
+lengaujeOut = ttk.Combobox(raiz, values=options)
+lengaujeOut.pack()
+
+#Slider para opacidad del recuadro negro
+background = Label(raiz, text="Background Opacity level", bg="gray")
+background.pack(pady=(30, 0))
+opacity = DoubleVar()
+slider = Scale(raiz, from_=0, to=100, orient="horizontal", variable=opacity)
+slider.pack()
+
+#Boton de Inicio
 btn_star = Button(raiz, text="Star Translation", command= Star_End)
-btn_star.pack()
+btn_star.pack(pady=(30, 0))
 
 raiz.mainloop()
---
+++ main.py
@@ -0,0 +1,3 @@
+from gui.setttings_window import raiz
+
+raiz.mainloop()
